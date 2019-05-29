f1db2c06d989cc6dfac769a7d1fb1d55138636f7
harjeevansingh <harjeevansingh01@gmail.com>
2019-05-29 16:03:25 +0530

    Authrntication working

--- halls/models.py
+++ halls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Hall(models.Model):
+    title = models.CharField(max_length=255)
+
+class Videos(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField(max_length=255)
+    youtube_id = models.CharField(max_length=255)
+    hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
