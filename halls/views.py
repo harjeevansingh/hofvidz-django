595971bd97a9054ee94a3e98775697bc4cc32803
harjeevansingh <harjeevansingh01@gmail.com>
2019-05-30 17:44:45 +0530

    added Create Hal

--- halls/views.py
+++ halls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .models import Hall
 
 
 def home(request):
@@ -12,3 +13,9 @@ class SignUp(generic.CreateView):
 	form_class = UserCreationForm
 	success_url = reverse_lazy('home')
 	template_name = 'registration/signup.html'
+
+class CreateHall(generic.CreateView):
+	model = Hall
+	fields = ['title']
+	template_name = 'halls/create_hall.html'
+	success_url = reverse_lazy('home')
--- hofvidz/urls.py
+++ hofvidz/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path('signup', views.SignUp.as_view(), name='signup'),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    #Views
+    path('halloffame/create', views.CreateHall.as_view(), name="create_hall"),
 ]
 
 urlpatterns += static(settings.STATIC_URL,documnt_root=settings.STATIC_ROOT)
